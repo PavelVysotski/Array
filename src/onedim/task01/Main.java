85647a4b6b836eb49a5a9e813b958ef5b46240d4
pvysotski <pavelve83@mail.ru>
2020-05-19 12:11:47 +0300

    update

--- src/onedim/task01/Main.java
+++ src/onedim/task01/Main.java
@@ -3,21 +3,27 @@ package onedim.task01;
 import java.util.Scanner;
 
 public class Main {
+    /*
+    В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+    */
     public static void main(String[] args) {
+        summa();
+    }
+
+    public static void summa() {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите размерность массива: ");
         int n = input.nextInt();
-        System.out.print("Введите число кратности: ");
-        int k = input.nextInt();
         int[] array = new int[n];
         int sum = 0;
-
+        System.out.print("Введите число кратности: ");
+        int k = input.nextInt();
         for ( int i = 0; i < n; i++ ) {
             array[i] = i + 1;
             if (array[i] % k == 0) {
                 sum = sum + array[i];
             }
         }
-        System.out.println("Сумма чисел кратные 'K' равна: " + sum);
+        System.out.println("Сумма чисел кратных 'K' равна: " + sum);
     }
 }
--- src/onedim/task02/Main.java
+++ src/onedim/task02/Main.java
@@ -1,8 +1,50 @@
 package onedim.task02;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    /*
+    Дана последовательность действительных чисел а1 ,а2 ,..., ап. Заменить все ее члены, большие данного Z, этим
+    числом. Подсчитать количество замен.
+    */
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размерность массива: ");
+        int n = input.nextInt();
+        System.out.println("Введите диапазон вставляемых чисел от 'a' до 'b': ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        double[] array = new double[n];
+
+        System.out.println("Введите число для замены: ");
+        double z = input.nextDouble();
+
+        createArray(array, a, b);
+        changeArray(array, z);
+    }
+
+    public static void createArray(double array[], int a, int b) {
+        for ( int i = 0; i < array.length; i++ ) {
+            array[i] = (Math.random() * (b - a)) + a;
+            System.out.printf("[" + i + "]: " + array[i] + ", ");
+        }
+        System.out.println();
+    }
+
+    public static int changeArray(double array[], double z) {
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Массив после замены.");
+        for ( int i = 0; i < array.length; i++ ) {
+            if (array[i] > z) {
+                array[i] = z;
+                count++;
+            }
+            System.out.print("[" + i + "]: " + array[i] + ", ");
+        }
+        System.out.println();
+        System.out.println("Количество замен равно: " + count);
+        return count;
     }
 }
