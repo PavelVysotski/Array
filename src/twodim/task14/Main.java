26856b1861555b19b0e7b6a78c17d63892232fb1
pvysotski <pavelve83@mail.ru>
2020-05-22 18:28:15 +0300

    update

--- src/twodim/task14/Main.java
+++ src/twodim/task14/Main.java
@@ -6,9 +6,12 @@ public class Main {
     единиц равно номеру столбца.
     */
     public static void main(String[] args) {
-        int m = (int) Math.round(Math.random() * 10 + 1);
-        int n = (int) Math.round(Math.random() * m);
+        showArray(createArray());
+    }
 
+    public static int[][] createArray() {
+        int m = (int) Math.round(Math.random() * (10 - 1) + 1);
+        int n = (int) Math.round(Math.random() * m);
         int[][] array = new int[m][n];
 
         for ( int i = 0; i < n; i++ ) {
@@ -16,7 +19,10 @@ public class Main {
                 array[j][i] = 1;
             }
         }
+        return array;
+    }
 
+    public static void showArray(int[][] array) {
         for ( int i = 0; i < array.length; i++ ) {
             for ( int j = 0; j < array[i].length; j++ ) {
                 System.out.print(array[i][j] + "\t");
