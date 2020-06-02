dfcc4ae578a914ba32ffcee1e226ddac84bc9493
pvysotski <pavelve83@mail.ru>
2020-06-02 12:45:08 +0300

    update

--- src/decomposition/task06/Main.java
+++ src/decomposition/task06/Main.java
@@ -5,6 +5,30 @@ public class Main {
     Написать метод(методы), проверяющий, являются ли данные три числа взаимно простыми.
     */
     public static void main(String[] args) {
+        int a = 5;
+        int b = 15;
+        int c = 20;
 
+
+        if (NOD3(a, b, c) == 1) {
+            System.out.println("Числа взаимно простые");
+        } else {
+            System.out.println("Числа не взаимно простые");
+        }
+    }
+
+    public static int NOD3(int a, int b, int c) {
+        return NOD(a, NOD(b, c));
+    }
+
+    public static int NOD(int a, int b) {
+        while (a != 0 && b != 0) {
+            if (a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        return a + b;
     }
 }
