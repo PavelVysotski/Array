dce4b25a5212316008f43f616440e4da6e30350d
pvysotski <pavelve83@mail.ru>
2020-06-02 15:18:41 +0300

    update

--- src/decomposition/task14/Main.java
+++ src/decomposition/task14/Main.java
@@ -1,5 +1,7 @@
 package decomposition.task14;
 
+import java.util.Scanner;
+
 public class Main {
     /*
     Натуральное число, в записи которого n цифр, называется числом Армстронга, если сумма его цифр,
@@ -7,6 +9,41 @@ public class Main {
     использовать декомпозицию.
     */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите 'k': ");
+        int k = input.nextInt();
 
+        showAllArmstrNumb(k);
+    }
+
+    public static void showAllArmstrNumb(int k) {
+        for ( int i = 1; i <= k; i++ ) {
+            if (isArmstrong(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isArmstrong(int n) {
+        int sum = 0;
+        int digit = 0;
+        int copy = n;
+        int length = lengthNumber(n);
+
+        while (n > 0) {
+            digit = n % 10;
+            sum += Math.pow(digit, length);
+            n = n / 10;
+        }
+        return sum == copy;
+    }
+
+    public static int lengthNumber(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
     }
 }
--- src/decomposition/task15/Main.java
+++ src/decomposition/task15/Main.java
@@ -1,11 +1,45 @@
 package decomposition.task15;
 
+import java.util.Scanner;
+
 public class Main {
     /*
     Найти все натуральные n-значные числа, цифры в которых образуют строго возрастающую
     последовательность (например, 1234, 5789). Для решения задачи использовать декомпозицию.
     */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число 'n': ");
+        int n = input.nextInt();
 
+        findNumbers(n);
     }
+
+    public static int[] createArray(int number, int n) {
+        int[] array = new int[n];
+        for ( int i = 0; i < n; i++ ) {
+            array[n - 1 - i] = number % 10;
+            number = number / 10;
+        }
+        return array;
+    }
+
+    public static void findNumbers(int n) {
+        int a = (int) Math.pow(10, n - 1);
+        int b = (int) (Math.pow(10, n) - 1);
+        for ( int i = a; i <= b; i++ ) {
+            int[] array = createArray(i, n);
+            int count = 0;
+            for ( int j = 1; j < n; j++ ) {
+                if (array[j] == array[j - 1] + 1) {
+                    count++;
+                }
+            }
+            if (count == n - 1) {
+                System.out.printf(i + ", ");
+            }
+        }
+    }
+
+
 }
